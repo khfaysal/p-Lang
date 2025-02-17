9ac02008b68f831d906cc21c77a9f52bf30361b7
Faysal <k.hasanfaysal@gmail.com>
2025-02-17 15:00:45 +0600

    InsertSort

---
+++ C++/4th Sem/InsertionSort.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int size;
+    cout << "enter the size of an array: ";
+    cin >> size;
+    int arr[size];
+    cout << "enter the elements: ";
+    for (int i = 0; i < size; i++)
+    {
+        cin >> arr[i];
+    }
+
+    for (int j = 1; j < size; j++)
+    {
+        int key = arr[j];
+        int i = j - 1;
+        while (key < arr[i] && i >= 0)
+        {
+            arr[i + 1] = arr[i];
+            i--;
+        }
+        arr[i + 1] = key;
+    }
+
+    cout << "Final Array: ";
+
+    for (int k = 0; k < size; k++)
+    {
+        cout << arr[k] << " ";
+    }
+
+    return 0;
+}
---
+++ C++/4th Sem/tempCodeRunnerFile.cpp
@@ -0,0 +1,12 @@
+ // for (int j = 1; i < n = ; i++)
+    // {
+    //     key = arr[j];
+    //     int i = j - 1;
+    //     while (key < arr[i] && i >= 0)
+    //     {
+    //         arr[i + 1] = arr[i];
+    //         i--;
+    //     }
+    //     arr[i + 1] = key;
+    //     ;
+    // }
