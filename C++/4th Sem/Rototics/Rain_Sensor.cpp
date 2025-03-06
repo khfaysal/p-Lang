06847b7c32d2a070b4e8cbe83251b196af8c1eff
Kamrul Hasan Faysal <k.hasanfaysal@gmail.com>
2025-03-06 11:21:33 +0600

    Update Rain_Sensor.cpp
    phone change

--- C++/4th Sem/Rototics/Rain_Sensor.cpp
+++ C++/4th Sem/Rototics/Rain_Sensor.cpp
@@ -1,4 +1,4 @@
-// Sensor pins pin D6 LED output, pin A0 analog Input
+
 #define ledPin 6
 #define sensorPin A0
 void setup() {
@@ -11,10 +11,10 @@ void loop() {
   Serial.println(readSensor());
   delay(500);
 }
-//  This function returns the analog data to calling function
+
 int readSensor() {
-  int sensorValue = analogRead(sensorPin);  // Read the analog value from sensor
-  int outputValue = map(sensorValue, 0, 1023, 255, 0); // map the 10-bit data to 8-bit data
-  analogWrite(ledPin, outputValue); // generate PWM signal
-  return outputValue;             // Return analog rain value
+  int sensorValue = analogRead(sensorPin);  
+  int outputValue = map(sensorValue, 0, 1023, 255, 0); 
+  analogWrite(ledPin, outputValue); 
+  return outputValue;             
 }
