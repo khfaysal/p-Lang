581a00eabf1aa0c5b1fcd3b343711da696cad360
Faysal <k.hasanfaysal@gmail.com>
2025-08-06 21:57:29 +0600

    js

---
+++ React/me1/src/Bio-Data.jsx
@@ -0,0 +1,22 @@
+const Biodata = () => {
+    return (
+        <div className="bio-data">
+            <h2> BioData of Kamrul Hasan Faysal</h2>
+            <hr />
+            <div className="personal-info">
+                <p>Phone: 012345</p>
+                <p>Email:44kamrulhasan@gmail.com
+                </p>
+                <p>Linkedin: khfaysal</p>
+            </div>
+            <div>
+                <h1>Skills</h1>
+                <ul>
+                    <li>C++</li>
+                    <li>JavaScript</li>
+                    <li>PHP</li>
+                </ul>
+            </div>
+        </div>
+    )
+}
---
+++ React/me1/zNote.md
@@ -0,0 +1,16 @@
+#  .jsx -> Java Script xml file
+
+
+### Components: [is nothing but a special type of function ]
+3 rule of Components:
+1. A Component must be a function
+2. That function should return something
+3. That something should be some html-ish code (JSX element)
+
+
+##  .jsx file means --> Component niye kaj hobe ba hoyeche.
+
+##  A component is a special type of function (or class) that returns JSX, which React uses to render UI.
+  ![alt text](image.png)
+
+##      Component ekta function, ar ai fuction ke reusable korte hoy tahole lage perameter, ei perameter ke bola hoy PROPS in react.
