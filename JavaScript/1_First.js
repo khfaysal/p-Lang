dcbb3eb71ed15106495cb53b14eaec071b1465ed
Faysal <k.hasanfaysal@gmail.com>
2025-12-05 22:08:35 +0600

    Second

--- JavaScript/1_First.js
+++ JavaScript/1_First.js
@@ -1,1 +1,9 @@
-console.log("Hiiiiiiiiii")
+let num = 400;
+// console.log(num);
+// console.log(num.toString());
+// console.log(num.toExponential());
+let num2 = 123.786;
+// console.log(num2.toFixed(2));
+console.log(Math.round(num2));
+console.log(Math.ceil(num2));
+console.log(Math.floor(num2));
