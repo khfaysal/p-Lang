2e85eff76e1a71cf2d276de6f4df422692ddbb03
Faysal <k.hasanfaysal@gmail.com>
2025-12-19 03:38:15 +0600

    Hudai

---
+++ C/Practice/Prac.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void)
+{
+
+    // printf("Hello, World!\n");
+    int a = 5, b = 10;
+
+    if (a == 5 && b == 10)
+    {
+        printf("Both conditions are true.\n");
+    }
+    return 0;
+}
